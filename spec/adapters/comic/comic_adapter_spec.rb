93a77e9bf6be98f2df8a3fd9a84ccbfc3b87a97f
Gabriel Mesquita <gabrieljustware@gmail.com>
2021-09-04 15:37:48 -0300

    Create first test for comic adapter

--- spec/adapters/comic/comic_adapter_spec.rb
+++ spec/adapters/comic/comic_adapter_spec.rb
@@ -1,16 +1,23 @@
 require 'rails_helper'
 require_relative '../../../app/adapters/comic/comic_adapter'
+require_relative '../../../app/entities/comic/comic'
 
 describe Adapters::Comic::ComicAdapter do
   context 'when adapting comic outside response' do
     it 'returns an internal entity representing a comic' do
-        comics = [{title: "deadpool", thumbnail: "thumbnail.com/1"},
-                  {title: "hulk", thumbnail: "thumbnail.com/2"}]
+        comics = [{title: "deadpool", thumbnail: {path: "thumbnail-1.com", extension: "jpg"}},
+                  {title: "hulk", thumbnail: {path: "thumbnail-2.com", extension: "jpg"}}]
         api_comics = {data: {results: comics}}.with_indifferent_access
+        first_comic = Entities::Comic::Comic.new(title: "deadpool",
+                                                 image: "thumbnail-1.com/portrait_fantastic.jpg")
+        second_comic = Entities::Comic::Comic.new(title: "hulk",
+                                                  image: "thumbnail-2.com/portrait_fantastic.jpg")
+        expected_result = [first_comic, second_comic]
 
         internal_comics = described_class.adapt(api_comics)
 
         expect(internal_comics.length).to be 2
+        expect(internal_comics).to eql(expected_result)
     end
   end
 end
